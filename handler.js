d828d66773f630d2851c7a63471995c7563e6224
Qendresa Hoti <qendresa@butlerhospitality.com>
2021-10-20 16:25:30 -0400

    sf example /wip

---
+++ handler.js
@@ -0,0 +1,15 @@
+const stepOneFunction = (input) => {
+    return {}
+}
+
+const stepTwoFunction = (input) => {
+    return {}
+}
+
+const executeStepFunction = (input) => {
+    return {}
+}
+
+module.exports.stepOneFuncton = stepOneFunction;
+module.exports.stepTwoFunction = stepTwoFunction;
+module.exports.executeStepFunction = executeStepFunction;
